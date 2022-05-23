f0f567749f03018c18ac9ad3fc6737092f2ee8ec
Brent <100012424+brentxr@users.noreply.github.com>
2022-05-23 01:15:23 -0400

    Updated controller and added wall jumps

--- Assets/_Scipts/PlayerController.cs
+++ Assets/_Scipts/PlayerController.cs
@@ -12,16 +12,31 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public int maxJumps;
 
+    private float _wallJumpCooldown = 1f;
+
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+
     private float _curMoveInput = 0;
     private int _jumpsAvailable;
+    private bool _isGrounded;
+    private BoxCollider2D _boxCollider;
 
-    [Header("Components")]
-    public Rigidbody2D rig;
+    private Rigidbody2D _rig;
 
+    private int _facing;
+
+
+    private void Awake()
+    {
+        _rig = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void FixedUpdate()
     {
         Move();
+        
     }
 
     public void OnCollisionEnter2D(Collision2D col)
@@ -32,29 +47,77 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        rig.velocity = new Vector2(_curMoveInput * moveSpeed, rig.velocity.y);
+        _rig.velocity = new Vector2(_curMoveInput * moveSpeed, _rig.velocity.y);
     }
 
     private void Jump()
     {
-        rig.velocity = new Vector2(rig.velocity.x, 0);
-        rig.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
+        if (IsGrounded())
+        {
+            _rig.velocity = new Vector2(_rig.velocity.x, 0);
+            _rig.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
+        }
+        else if (IsOnWall() && !IsGrounded())
+        {
+            if (_curMoveInput == 0)
+            {
+                _rig.velocity = new Vector2(-Mathf.Sign(_facing) * 10, 6);
+                _facing *= -1;
+            }
+            else
+            {
+                _rig.velocity = new Vector2(-Mathf.Sign(_facing) * 3, 6);
+            }
+            
+            _wallJumpCooldown = 0;
+            
+        }
+        
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            _boxCollider.bounds.center, 
+            _boxCollider.bounds.size, 
+            0, 
+            Vector2.down, 
+            0.1f, 
+            groundLayer); 
+        
+        return hit.collider != null;
+    }
+    
+    private bool IsOnWall()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            _boxCollider.bounds.center, 
+            _boxCollider.bounds.size, 
+            0, 
+            new Vector2(transform.localScale.x, 0), 
+            0.1f, 
+            wallLayer);
+        
+        //Debug.Log("Is OnWall");
+        return hit.collider != null;
     }
 
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         _curMoveInput = context.ReadValue<float>();
+
+        if (_curMoveInput > 0.01f)
+            _facing = 1;
+        else if (_curMoveInput < -0.01f)
+            _facing = -1;
     }
 
     public void OnJumpInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
-            if (_jumpsAvailable > 0)
-            {
-                _jumpsAvailable--;
-                Jump();
-            }
+            Jump();
+           
         }
     }
     
